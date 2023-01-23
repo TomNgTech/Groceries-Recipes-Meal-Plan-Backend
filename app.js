8bacb5d6a188398ed0516ffa67d600b51528370b
Tung Nguyen <tnguyen7@fredhutch.org>
2023-01-23 15:04:34 -0800

    Set up DynamooseDB and remove node_modules from git

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// Set up DB Connection
+const dynamoose = require("dynamoose");
+const ddb = new dynamoose.aws.ddb.DynamoDB({
+    "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+    "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+    "region": process.env.AWS_REGION
+});
+dynamoose.aws.ddb.set(ddb);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var recipesRouter = require('./routes/recipes');
 
 var app = express();
 
@@ -16,5 +28,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/recipes', recipesRouter);
 
 module.exports = app;
