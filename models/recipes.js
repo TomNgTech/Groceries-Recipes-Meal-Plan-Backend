7f98dfbf1864ee32cb7b39c1869f8a5a9c55f394
Diana K <dianaxkang@gmail.com>
2023-01-25 19:47:33 -0800

    Added measurementType

--- models/recipes.js
+++ models/recipes.js
@@ -2,16 +2,16 @@ const dynamoose = require("dynamoose");
 
 const recipeSchema = new dynamoose.Schema(
     {
-        id: { type: String, required: true, hashKey: true },
-        dishName: { type: String, required: true },
-        ingredients: [
-            {
-                name: { type: String, required: true },
-                quantity: { type: String, required: true },
-            }
-        ],
-        servingSize: { type: Number, required: true },
-    },
+         id: { type: String, required: true },
+    dishName: { type: String, required: true },
+    ingredients: [
+        {
+            name: { type: String, required: true },
+            quantity: { type: String, required: true },
+            measurementType: { type: String, required: true },
+        }
+    ],
+    servingSize: { type: Number, required: true },
     {
         saveUnknown: true,
         timestamps: true,
