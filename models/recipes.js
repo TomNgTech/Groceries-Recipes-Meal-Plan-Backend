25806ca2466b97703671a7bb5fca212cb18ec746
Diana K <dianaxkang@gmail.com>
2023-01-26 16:20:53 -0800

    Fixed Spacing
    fixed spacing

--- models/recipes.js
+++ models/recipes.js
@@ -2,13 +2,13 @@ const dynamoose = require("dynamoose");
 
 const recipeSchema = new dynamoose.Schema(
     {
-         id: { type: String, required: true },
+    id: { type: String, required: true },
     dishName: { type: String, required: true },
     ingredients: [
         {
-            name: { type: String, required: true },
-            quantity: { type: String, required: true },
-            measurementType: { type: String, required: true },
+        name: { type: String, required: true },
+        quantity: { type: String, required: true },
+        measurementType: { type: String, required: true },
         }
     ],
     servingSize: { type: Number, required: true },
