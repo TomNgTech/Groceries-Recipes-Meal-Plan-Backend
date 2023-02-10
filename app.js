61f981b3ec8a7d4e022c3f9bc226cd143757b88b
Tung Nguyen <tnguyen7@fredhutch.org>
2023-02-09 16:04:32 -0800

    Add plural to /mealPlans

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 app.use('/recipes', recipesRouter)
-app.use('/mealPlan', mealPlanRouter)
+app.use('/mealPlans', mealPlanRouter)
 app.use('/ingredients', ingredientsRouter)
 
 module.exports = app
