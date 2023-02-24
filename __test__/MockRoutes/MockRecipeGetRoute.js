30606d50a27e3cccfc6734eb20ccbf0c49a4d687
josh671 <49129783+josh671@users.noreply.github.com>
2023-02-23 18:46:17 -0800

    changed get test methods

---
+++ __test__/MockRoutes/MockRecipeGetRoute.js
@@ -0,0 +1,21 @@
+const axios = require('axios')
+const url = require('../../axiosConfig')
+module.exports = {
+  async getRecipes() {
+    try {
+      const response = await axios.get(url)
+      return response
+    } catch (err) {
+      return err
+    }
+  },
+
+  async getRecipesById(recipeId) {
+    try {
+      const response = await axios.get(url + `/${recipeId}`)
+      return response[0]
+    } catch (err) {
+      return err
+    }
+  },
+}
